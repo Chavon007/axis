2a072dfaae70dbd7689324bd8dd8a6911e48b749
azuh salvation <salvationazuh@gmail.com>
2026-03-15 22:24:50 +0100

    added room details

--- types/hotelType.tsx
+++ types/hotelType.tsx
@@ -1,8 +1,29 @@
+import hotelRooms from "@/data/hotelRoom";
+
+interface Room {
+  id: string;
+  name: string;
+  floor: number;
+  price: number;
+  size: string;
+  maxGuests: number;
+  beds: string;
+  images: any[];
+  description: string;
+  amenities: string[];
+}
+
+interface RoomCategories {
+  standard: Room[];
+  semiDeluxe: Room[];
+  deluxe: Room[];
+}
+
 interface HotelDetailsListing {
+  id: string;
   name: string;
   address: string;
   rating: string;
-  roomType: string[];
   image: any;
   owner: string;
   customerServiceContact: string;
@@ -12,6 +33,10 @@ interface HotelDetailsListing {
     twitter?: string;
   };
   about: string;
+  rooms: RoomCategories;
 }
 
+export type { Room, RoomCategories };
 export default HotelDetailsListing;
+export type HotelName = keyof typeof hotelRooms;
+export type RoomType = keyof RoomCategories;
---
+++ utils/hotelHelpeer.tsx
@@ -0,0 +1,18 @@
+import hotelRooms from "@/data/hotelRoom";
+import { HotelName, RoomType, Room } from "../types/hotelType";
+
+export const getRoom = (hotelRoom: HotelName, roomType: RoomType): Room[] => {
+  const hotel = hotelRooms[hotelRoom];
+
+  if (!hotel) return [];
+
+  return hotel[roomType] ?? [];
+};
+
+export const getAllRooms = (hotelRoom: HotelName): Room[] => {
+  const allHotels = hotelRooms[hotelRoom];
+
+  if (!allHotels) return [];
+
+  return [...allHotels.deluxe, ...allHotels.semiDeluxe, ...allHotels.standard];
+};
