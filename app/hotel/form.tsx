89505fb4a88d6d29325d3e507adf0447818508ad
azuh salvation <salvationazuh@gmail.com>
2026-03-17 11:58:45 +0100

    added form validation

--- app/hotel/form.tsx
+++ app/hotel/form.tsx
@@ -2,10 +2,11 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { TextInput } from "react-native-gesture-handler";
 import { useState } from "react";
+import { useRouter } from "expo-router";
 import {  Form } from "@/types/formtype";
 
 export default function Form() {
-
+    const router = useRouter();
     const [formData, setFormData] = useState<Form>({
         fullName:"", email:"", number:0, nin:"", guest:1, request:""
     })
@@ -13,6 +14,33 @@ export default function Form() {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState("")
     const [success, setSuccess] = useState("")
+
+
+    const handleSubmit = () => {
+    setLoading(true)
+    setError("")
+    setSuccess("")
+        if(!formData.email || !formData.fullName || !formData.guest || !formData.nin || !formData.number){
+            setError("Please fill all required fields")
+            setLoading(false)
+            return
+        }  if(! /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)){
+            setError("Please use a valid email address")
+            setLoading(false)
+            return
+        } 
+         if( formData.nin.length !== 11 ){
+            setError("NIN must be exactly 11 digits")
+            setLoading(false)
+            return
+        }
+       setTimeout(() => {
+                setSuccess("Personal details submitted successfully")
+                setLoading(false)
+                router.push("/booking")
+            }, 3000)
+        
+    }
   return (
     <View>
       <Text>Your details</Text>
