613801ea89365643bd89092156e934077591022c
azuh salvation <salvationazuh@gmail.com>
2026-03-17 10:57:55 +0100

    added the personal detail page

--- app/hotel/form.tsx
+++ app/hotel/form.tsx
@@ -1,8 +1,18 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { TextInput } from "react-native-gesture-handler";
+import { useState } from "react";
+import {  Form } from "@/types/formtype";
 
 export default function Form() {
+
+    const [formData, setFormData] = useState<Form>({
+        fullName:"", email:"", number:0, nin:"", guest:1, request:""
+    })
+
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
   return (
     <View>
       <Text>Your details</Text>
---
+++ types/formtype.tsx
@@ -0,0 +1,8 @@
+export interface Form {
+  fullName: string;
+  number: number;
+  email: string;
+  nin: string;
+  guest: number;
+  request: string;
+}
