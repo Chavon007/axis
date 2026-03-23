0fd4b54b179c4820102cb466dd2f22f180dbed19
azuh salvation <salvationazuh@gmail.com>
2026-03-23 23:58:02 +0100

    working on the booking page

--- components/bookingCard.tsx
+++ components/bookingCard.tsx
@@ -1,21 +1,45 @@
-import { View, Text, Pressable} from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import { Bookings } from "@/types/hotelType";
 import { useRouter } from "expo-router";
 interface BookingCardProps {
-  booking: Bookings
-
+  booking: Bookings;
 }
-export default function BookingCard({ booking}: BookingCardProps) {
+export default function BookingCard({ booking }: BookingCardProps) {
   const router = useRouter();
   return (
+    <Pressable
+      onPress={() =>
+        router.push({
+          pathname: "/hotel/receipt",
+          params: {
+            hotelName: booking.hotelName,
+            roomId: booking.roomId,
+            roomName: booking.roomName,
+            checkInDate: booking.checkInDate,
+            checkOutDate: booking.checkOutDate,
+          },
+        })
+      }
+    >
+      <View>
+        <Text>{booking.roomId}</Text>
+        <View>
+          <Text>{booking.roomName}</Text>
+          <Text>{booking.hotelName}</Text>
+        </View>
+        <View>
+          <View>
+            <Text>Check-in</Text>
+            <Text>{booking.checkInDate}</Text>
+          </View>
 
-
-    <Pressable onPress={() => router.push({ pathname: "/hotel/receipt", params:{
-
-    }})}>
-
+          <View>
+            <Text>Check-out</Text>
+            <Text>{booking.checkOutDate}</Text>
+          </View>
+        </View>
+      </View>
     </Pressable>
-   
   );
 }
