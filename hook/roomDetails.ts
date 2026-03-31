27061673882bf654d49a1af33acb00835c88a905
azuh salvation <salvationazuh@gmail.com>
2026-03-31 18:36:44 +0100

    added the fetch room details function

---
+++ hook/roomDetails.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+import Constants from "expo-constants";
+
+const supabase_url = Constants.expoConfig?.extra?.MY_SUPABASE_URL ?? "";
+const project_key = Constants.expoConfig?.extra?.MY_PROJECT_KEY ?? "";
+
+const supabase = createClient(supabase_url, project_key);
+
+const getRoomDetails = async () => {
+  const { data, error } = await supabase.from("hotelrooms").select("id, hotel_id, name, floor, priice, size,maxGuests, beds, images,description, amenities, room_type ");
+
+  if (error) {
+    console.error("Error in fetching room details", error);
+    return [];
+  }
+
+  return data;
+};
+
+export default getRoomDetails;
