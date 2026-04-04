9f5b54c971aed86f66ee607874c3e8e339199235
azuh salvation <salvationazuh@gmail.com>
2026-04-04 23:10:07 +0100

    added the checkindate/checkout url from he backend

---
+++ hook/calendarHook.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { BookingDate } from "@/types/datetype";
+export const calculatePrice = (calculate?: BookingDate) => {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const mainPrice = async (calculate: BookingDate) => {
+    setLoading(true);
+    setError("");
+    setSuccess("");
+    try {
+      const res = await fetch("http://localhost:5000/api/checkindate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(calculate),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setError("Can't send data now");
+        return;
+      }
+      setSuccess("Succussful");
+      setResult(data);
+      return data;
+    } catch (err) {
+      setError("Faileed to calculate price");
+      return;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (calculate) {
+      mainPrice(calculate);
+    }
+  }, [calculate]);
+
+  return { loading, success, error, result };
+};
