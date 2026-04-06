97182c68d24bcb32524cfc570c21d049f5daf11b
azuh salvation <salvationazuh@gmail.com>
2026-04-06 21:20:48 +0100

    passed the eamil to the params from the form

--- app/hotel/form.tsx
+++ app/hotel/form.tsx
@@ -72,6 +72,7 @@ export default function FormPage() {
             price,
             fullName: formData.fullName,
             roomName,
+            email: formData.email,
           },
         });
       } else {
